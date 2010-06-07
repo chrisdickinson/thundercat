4fec548705408292b8d07839f7bd778ffed13c52
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-06-07 11:48:15 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+import os
+import glob
+
+setup(
+    name = "thundercat",
+    version = "0.1",
+    packages = find_packages(),
+
+    author = "Chris Dickinson",
+    author_email = "chris@neversaw.us",
+    description = "python client app for nappingcat framework servers",
+    long_description = """
+A command line interface to nappingcat servers. Allows the user to create, fork, and delete repositories from the command line.
+""".strip(),
+    license = "BSD",
+    keywords = "ssh git client",
+    url = "http://github.com/chrisdickinson/thundercat/",
+
+    entry_points = {
+        'console_scripts': [
+                'thundercat = thundercat.main:main',
+            ],
+        },
+
+    zip_safe=False,
+    install_requires=[
+        'setuptools>=0.6c5',
+        'importlib>=1.0.2',
+        'paramiko>=1.7.4',
+        ],
+    )
+
+
